80f7f1e9c1adec9215a5ef81adf3bd8bc276731a
Robert Peteuil <robert@peteuil.com>
2017-06-02 16:41:58 -0700

    Adjusted Table Displays for New Formats

--- mcc/dispout.py
+++ mcc/dispout.py
@@ -27,27 +27,50 @@ from mcc.colors import C_NORM, C_TI, C_STAT
 from prettytable import PrettyTable
 
 
-def print_table(all_nodes):
-    """Print Table for OLD flat-list all_nodes."""
-    h_name = C_TI + "NAME"
-    h_state = "STATE" + C_NORM
-    nt = PrettyTable()
-    nt.header = False
-    nt.add_row([h_name, "REGION", "CLOUD", "SIZE", "PUBLIC IP", h_state])
-    nt.padding_width = 2
-    nt.border = False
-    for node in all_nodes:
-        state = C_STAT[node.state] + node.state + C_NORM
-        if node.public_ips:
-            n_ip = node.public_ips
-        else:
-            n_ip = "-"
-        nt.add_row([node.name, node.zone, node.cloud, node.size,
-                    n_ip, state])
-    print(nt)
+# def print_table(all_nodes):
+#     """Print Table for OLD flat-list all_nodes."""
+#     h_name = C_TI + "NAME"
+#     h_state = "STATE" + C_NORM
+#     nt = PrettyTable()
+#     nt.header = False
+#     nt.add_row([h_name, "REGION", "CLOUD", "SIZE", "PUBLIC IP", h_state])
+#     nt.padding_width = 2
+#     nt.border = False
+#     for node in all_nodes:
+#         state = C_STAT[node.state] + node.state + C_NORM
+#         if node.public_ips:
+#             n_ip = node.public_ips
+#         else:
+#             n_ip = "-"
+#         nt.add_row([node.name, node.zone, node.cloud, node.size,
+#                     n_ip, state])
+#     print(nt)
 
 
-def print_list_table(all_nodes):
+# def print_indx_table(all_nodes):
+#     """Convert flat-list all_nodes to dict then print table."""
+#     node_list = {}
+#     for i, j in enumerate(all_nodes):
+#         node_list[i] = j
+#     h_nm = C_TI + "NUM"
+#     h_state = "STATE" + C_NORM
+#     nt = PrettyTable()
+#     nt.header = False
+#     nt.add_row([h_nm, "NAME", "REGION", "CLOUD", "SIZE", "PUBLIC IP", h_state])
+#     nt.padding_width = 2
+#     nt.border = False
+#     for i, node in node_list.items():
+#         state = C_STAT[node.state] + node.state + C_NORM
+#         if node.public_ips:
+#             n_ip = node.public_ips
+#         else:
+#             n_ip = "-"
+#         nt.add_row([i + 1, node.name, node.zone, node.cloud, node.size,
+#                     n_ip, state])
+#     print(nt)
+
+
+def print_table2(all_nodes):
     """Print Table for NEW nested-list all_nodes."""
     h_name = C_TI + "NAME"
     h_state = "STATE" + C_NORM
@@ -68,11 +91,8 @@ def print_list_table(all_nodes):
     print(nt)
 
 
-def print_indx_table(all_nodes):
-    """Convert flat-list all_nodes to dict then print table."""
-    node_list = {}
-    for i, j in enumerate(all_nodes):
-        node_list[i] = j
+def print_indx_table2(node_dict):
+    """Print Table for NEW dict=formatted list."""
     h_nm = C_TI + "NUM"
     h_state = "STATE" + C_NORM
     nt = PrettyTable()
@@ -80,12 +100,12 @@ def print_indx_table(all_nodes):
     nt.add_row([h_nm, "NAME", "REGION", "CLOUD", "SIZE", "PUBLIC IP", h_state])
     nt.padding_width = 2
     nt.border = False
-    for i, node in node_list.items():
+    for i, node in node_dict.items():
         state = C_STAT[node.state] + node.state + C_NORM
         if node.public_ips:
             n_ip = node.public_ips
         else:
             n_ip = "-"
-        nt.add_row([i + 1, node.name, node.zone, node.cloud, node.size,
+        nt.add_row([i, node.name, node.zone, node.cloud, node.size,
                     n_ip, state])
     print(nt)
