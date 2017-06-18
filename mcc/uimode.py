f32f8b083864c134f22712bfe93c5b83804f4e6a
Robert Peteuil <robert@peteuil.com>
2017-06-17 20:54:52 -0700

    Added Code and Framework for Connect and Details Commands

--- mcc/uimode.py
+++ mcc/uimode.py
@@ -40,6 +40,8 @@ def ui_main(fmt_table, node_dict):
     cmd_action = {"quit": False,
                   "run": node_cmd,
                   "stop": node_cmd,
+                  "connect": node_cmd,
+                  "details": node_cmd,
                   "update": True}
     uiprint("\033[?25l")  # cursor off
     print("{}\n".format(fmt_table))
@@ -63,7 +65,8 @@ def ui_main(fmt_table, node_dict):
 def get_cmd(node_dict):
     """Get main command selection."""
     key_lu = {"q": ["quit", True], "r": ["run", True],
-              "s": ["stop", True], "u": ["update", True]}
+              "s": ["stop", True], "u": ["update", True],
+              "c": ["connect", True], "d": ["details", True]}
     disp_cmd_bar()
     cmd_valid = False
     flush_input()
@@ -80,16 +83,20 @@ def get_cmd(node_dict):
 
 def node_cmd(cmd_todo, node_dict):
     """Process commands that target specific nodes."""
+    sc = {"run": cmd_startstop, "stop": cmd_startstop,
+          "connect": cmd_conn, "details": cmd_details}
     inst_num = tar_selection(cmd_todo, len(node_dict))
     refresh_main = None
     if inst_num != 0:
         (tar_valid, tar_mess) = tar_validate(node_dict, inst_num, cmd_todo)
         if tar_valid:
-            # call function to get dynamic sub-command for commands
+            # get dynamic sub-command for commands
             #   that use the node logic up to this point, but now deviate
             #   like CONNECT and DETAILS
             # line below will call the returned sub-command dynamically
-            cmd_result = cmd_exec(node_dict[inst_num], cmd_todo, tar_mess)
+            subcmd = sc[cmd_todo]
+            cmd_result = subcmd(node_dict[inst_num], cmd_todo, tar_mess)
+            # cmd_result = cmd_startstop(node_dict[inst_num],cmd_todo,tar_mess)
             if cmd_result != "Command Aborted":
                 refresh_main = True
                 c_result = C_GOOD
@@ -159,10 +166,10 @@ def tar_validate(node_dict, inst_num, cmdname):
     return (tar_valid, tar_mess)
 
 
-def cmd_exec(tar_node, cmdname, tar_mess):
+def cmd_startstop(tar_node, cmdname, tar_mess):
     """Confirm command and execute it."""
-    cmd_lu = {"run": ["ex_start_node", "wait_until_running", "Successfull"],
-              "stop": ["ex_stop_node", "", "Initiated"]}
+    cmd_lu = {"run": ["ex_start_node", "wait_until_running", "Success"],
+              "stop": ["ex_stop_node", "", "Init"]}
     delay_lu = {"azure": {"stop": 5}}
     endms_lu = {"azure": {"stop": "Initiated"}}
     conf_mess = ("\r{0} - Confirm [y/N]: ".
@@ -196,6 +203,18 @@ def cmd_exec(tar_node, cmdname, tar_mess):
     return cmd_result
 
 
+def cmd_conn(tar_node, cmd_todo, tar_mess):
+    """Connect to node."""
+    cmd_result = "Command Aborted"
+    return cmd_result
+
+
+def cmd_details(tar_node, cmd_todo, tar_mess):
+    """Display Node details."""
+    cmd_result = "Command Aborted"
+    return cmd_result
+
+
 def input_yn(conf_mess):
     """Print Confirmation Message and Get Y/N response from user."""
     disp_erase_ln()
@@ -225,9 +244,14 @@ def disp_cmd_title(cmd_title):
 
 def disp_cmd_bar():
     """Display Command Bar."""
-    cmd_bar = ("\rSELECT COMMAND -   {2}(R){1}un Node   {3}"
-               "(S){1}top Node   {5}(U){1}pdate Info    {4}(Q){1}uit: ".
-               format(C_TI, C_NORM, C_GOOD, C_ERR, MAGENTA, C_WARN))
+    cmd_bar = ("\rSELECT COMMAND -  {2}(R){1}un   {0}(C){1}onnect   "
+               "{3}(S){1}top   {0}(U){1}pdate Info"
+               "   {5}(Q){1}uit: ".
+               format(C_TI, C_NORM, C_GOOD, C_ERR, MAGENTA))
+    # cmd_bar = ("\rSELECT COMMAND -  {2}(R){1}un   {0}(C){1}onnect   "
+    #            "{3}(S){1}top   {0}(D){1}etails   {0}(U){1}pdate Info"
+    #            "   {5}(Q){1}uit: ".
+    #            format(C_TI, C_NORM, C_GOOD, C_ERR, MAGENTA))
     disp_erase_ln()
     uiprint(cmd_bar)
 
