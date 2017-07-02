962e6c2e7d6e2c1124c0f7c2fb2920d96c6a139f
Robert Peteuil <robert@peteuil.com>
2017-07-02 14:34:23 -0700

    Adjust requires for systems with Py < 2.7.9 and openssl < 1.0.2
    - On these systems, “certifi” would generate a “certificate verify failed” error for SSL3_GET_SERVER_CERTIFICATE
      - this occurs when logging into GCP
    - Workaround is to install “certifi” <= 2015.9.6.2 and old “requests” <= 2.7.0
      - newer version of “requests” require newer versions of  “certifi”
    - Known OS with this situation is CentOS 7 with Python 2.7.5 and OpenSSL 1.0.1e
      - openssl distributed via OS package manager - so not upgradable via setup
    - Discussion of this issue: https://github.com/certifi/python-certifi/issues/26

--- setup.py
+++ setup.py
@@ -24,17 +24,22 @@ INSTALL_REQUIRES = ['apache-libcloud>=2.0.0',
                     'future>=0.14',
                     'gevent>=1.2.2',
                     'pycrypto>=2.6.1',
-                    'requests>=2.5.1',
+                    # 'requests >=2.5.1, <=2.7.0',
                     'PrettyTable>=0.7.2']
 
 EXTRAS_REQUIRE = {
     ":python_full_version<'2.7.9'": [
+        "requests >=2.5.1, <=2.7.0",
+        "certifi <= 2015.9.6.2",
         "pyOpenSSL>=17.0.0",
         "lxml>=3.8",
         "cssselect>=1.0.1",
         "ndg-httpsclient>=0.4.0",
         "pyasn1>=0.2.3",
         "backports.ssl_match_hostname>=3.5.0"
+    ],
+    ":python_full_version>='2.7.9'": [
+        "requests >= 2.5.1"
     ]
 }
 
@@ -42,13 +47,17 @@ if int(setuptools.__version__.split(".", 1)[0]) < 18:
     assert "bdist_wheel" not in sys.argv, "setuptools 18 required for wheels."
     # For legacy setuptools + sdist.
     if sys.version_info[0:3] <= (2, 7, 8):
+        INSTALL_REQUIRES.append("requests >=2.5.1, <=2.7.0")
+        INSTALL_REQUIRES.append("certifi <= 2015.9.6.2")
+        INSTALL_REQUIRES.append("pyOpenSSL>=17.0.0")
         INSTALL_REQUIRES.append("lxml>=3.8")
         INSTALL_REQUIRES.append("cssselect>=1.0.1")
         INSTALL_REQUIRES.append("ndg-httpsclient>=0.4.0")
-        INSTALL_REQUIRES.append("pyOpenSSL>=17.0.0")
         INSTALL_REQUIRES.append("pyasn1>=0.2.3")
         INSTALL_REQUIRES.append("backports.ssl_match_hostname>=3.5.0")
         EXTRAS_REQUIRE = {}
+    else:
+        INSTALL_REQUIRES.append("requests >= 2.5.1")
 
 setup(
     name='mcc',
@@ -56,7 +65,7 @@ setup(
     package_data={'mcc': ['config.ini']},
     entry_points={'console_scripts': ['mcc=mcc.core:main',
                                       'mccl=mcc.core:list_only']},
-    version='0.0.51',
+    version='0.0.53',
     author="Robert Peteuil",
     author_email="robert.s.peteuil@gmail.com",
     url='https://github.com/robertpeteuil/multi-cloud-control',
